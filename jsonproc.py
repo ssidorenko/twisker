952cd72ac7ae4b8079c814bcf226265fb457ad51
Semion Sidorenko <semion@rolepoint.com>
2013-03-20 17:56:45 +0000

    Added creation and deletion operations on twisks

--- jsonproc.py
+++ jsonproc.py
@@ -1,12 +1,11 @@
 import datetime
 import time
 import json
+from functools import wraps
+
+from flask import Response
 
 from google.appengine.ext import db
-
-def query_to_json(query_obj):
-    """Converts a gql query set, or a single model, respectively to JSON"""
-    return GAEJSONEncoder().encode(query_obj)
 
 class GAEJSONEncoder(json.JSONEncoder):
     """This json encoder parses models as dicts, with related models as id"""
@@ -14,15 +13,17 @@ class GAEJSONEncoder(json.JSONEncoder):
         # If it's a model, convert its properties to a dictionnary, which will
         # itself be parsed by the JSONEncoder later
         if isinstance(o, db.Model):
-            output = {'id':o.key().id_or_name()}
+            output = {}
             for key, prop in o.properties().iteritems():
                 value = getattr(o,key) 
                 if isinstance(value, db.Model):
-                    value = value.key().id_or_name()
+                    value = str(value.key().id_or_name())
                 elif isinstance(value, db.Key):
                     value = value.id_or_name()
 
                 output[key] = value
+                
+            output['id'] = str(o.key().id_or_name())
             return output
         elif isinstance(o, db.Query) or isinstance(o, db.GqlQuery):
             return list(o)
@@ -37,3 +38,21 @@ class GAEJSONEncoder(json.JSONEncoder):
             return int(ms)
         else:
             raise ValueError('cannot encode ' + repr(o))
+
+def json_query_view(f):
+    """Small decorators that runs the output of the passed function through the
+    json serializer and through a flask json response"""
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        return json_response(
+            GAEJSONEncoder().encode(
+                f(*args, **kwargs)
+            )
+        )
+
+    return decorated
+
+def json_response(data):
+    """Return a Flask response with JSON mimetype"""
+    resp = Response(data, status=200, mimetype='application/json')
+    return resp
--- twisker/views.py
+++ twisker/views.py
@@ -1,20 +1,39 @@
+import logging
+import json
+
+from flask import Response, request
+from jsonproc import json_query_view, GAEJSONEncoder
+
+from google.appengine.ext import db
+
 from twisker import app
 from models import Twisk
-from flask import Response
-from jsonproc import query_to_json
 
-@app.route('/twisks')
-def list_twisks():
-    twisks = Twisk.gql("ORDER BY when DESC")
+@app.route('/twisks/', methods=['GET', 'POST'])
+@json_query_view
+def twisks():
+    if request.method == 'GET':
+        twisks = Twisk.gql("ORDER BY when DESC")
+        return twisks
+    elif request.method == 'POST':
+        logging.debug(request.data)
+        data = json.loads(request.data)
 
-    return json_response(query_to_json(twisks))
+        twisk = Twisk(
+            author=db.Key.from_path("TwiskUser", data['author']),
+            content=data['content']
+        )
 
-@app.route('/twisk/<int:id>')
+        twisk.put()
+
+        return twisk
+
+@app.route('/twisks/<string:id>', methods=['GET','DELETE'])
+@json_query_view
 def get_twisk(id):
-    Twisk.get_by_id(id).content
+    twisk = Twisk.get_by_id(int(id))
 
-def json_response(data):
-    """Return a Flask response with JSON mimetype"""
-    resp = Response(data, status=200, mimetype='application/json')
-    print data
-    return resp
+    if request.method == 'DELETE':
+        twisk.delete()
+    else:
+        return twisk.content
